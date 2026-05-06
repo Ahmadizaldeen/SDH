c2ee32bf656bde2c7cbbe0e049bea0c157419d13
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-05-06 13:19:19 +0200

    refactor: Replace base_url.php with bootstrap.php for consistent configuration loading across files, remove require_once statment

--- include/templates/logout_form.php
+++ include/templates/logout_form.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ ."/../../config/base_url.php";
+require_once __DIR__ ."/../../config/bootstrap.php";
 ?>
 <form method="POST" action="<?= BASE_URL ?>/include/funktionen/logout.php" onsubmit="return confirm('Wirklich abmelden?')">
     <button type="submit">Abmelden</button>
